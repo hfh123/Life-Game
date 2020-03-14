78d86902780229e60d75e24afebdc123a97cdb61
hfh <2207837043@qq.com>
2020-03-14 12:43:22 +0800

    life game

---
+++ LifeGame.cpp
@@ -0,0 +1,24 @@
+#define High 20  //游戏尺寸
+#define Width 40
+int cellsmap[High][Width]; 
+int newcellsmap[High][Width];   
+void rawdate()  //游戏数据初始化
+{
+    int i,j;
+    for(i=0;i<High+1;i++)
+        for(j=0;j<Width+1;j++)
+            cellsmap[i][j]=1;   //细胞初始化状态为活细胞 
+}
+void RunLifeGame()   //进行游戏
+{
+    
+         
+    }
+}
+int main()
+{
+    
+}
+
+
+
